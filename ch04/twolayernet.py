fb34282d068a1c562843468a0f4ede3925e6297b
timmy1028 <timmy1028@gmail.com>
2017-05-05 17:19:50 +0900

    Add twolayernet.py

--- ch04/twolayernet.py
+++ ch04/twolayernet.py
@@ -56,6 +56,9 @@ class TwoLayerNet:
         
         return grads
         
+import datetime
+a = datetime.datetime.now().replace(microsecond=0)
+
 
 net = TwoLayerNet(input_size=784, hidden_size=100, output_size=10)
 print(net.params['W1'].shape)
@@ -63,4 +66,16 @@ print(net.params['b1'].shape)
 print(net.params['W2'].shape)
 print(net.params['b2'].shape)
 
+x = np.random.rand(100, 784)
+y = net.predict(x)
+x = np.random.rand(100, 784)
+t = np.random.rand(100, 10)
 
+grads = net.numerical_gradient(x, t)
+
+print(grads['W1'].shape)
+print(grads['b1'].shape)
+print(grads['W2'].shape)
+print(grads['b2'].shape)
+b = datetime.datetime.now().replace(microsecond=0)
+print(b-a)
